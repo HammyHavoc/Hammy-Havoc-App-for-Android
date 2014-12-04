1becc95ded03f557766be68d63939d25ea32ee20
Hammy Havoc <hammy@previousmagazine.com>
2014-12-04 17:40:16 +0000

    First export from WP-AppKit.

---
+++ config.js
@@ -0,0 +1,15 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'hammy-havoc-ios',
+		wp_ws_url : 'https://www.hammyhavoc.com/app-kit-api/hammy-havoc-ios',
+		theme : 'nihao-ios',
+		version : '1.0',
+		app_title : 'Hammy Havoc',
+		debug_mode : 'off',
+		auth_key : 'c9p%FYK|H{E[*R)r|c!yrqP*^nQi2!LeEz[wr|{[x[& 9kw+7Uv(D?`=J2MLi^ T',
+		options : {"refresh_interval":0}	};
+
+});
