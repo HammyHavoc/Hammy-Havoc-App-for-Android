bd875d72397d35e9d5a94a0ed070f5039218afd8
Hammy Havoc <hammy@splitanatom.com>
2016-07-26 04:00:07 +0100

    Updated UX.

--- config.js
+++ config.js
@@ -5,12 +5,16 @@ define( function ( require ) {
 	return {
 		app_slug : 'hammy-havoc-android',
 		wp_ws_url : 'https://www.hammyhavoc.com/wp-appkit-api/hammy-havoc-android',
-		theme : 'wpak-tabs-android',
-		version : '1.3',
+		wp_url : 'https://www.hammyhavoc.com',
+		theme : 'q-android',
+		version : '2.0',
 		app_title : 'Hammy Havoc',
+		app_platform : 'android',
+		gmt_offset : 0,
 		debug_mode : 'off',
 		auth_key : 'c9p%FYK|H{E[*R)r|c!yrqP*^nQi2!LeEz[wr|{[x[& 9kw+7Uv(D?`=J2MLi^ T',
 		options : {"refresh_interval":0},
+		theme_settings : [],
 		addons : []
 	};
 
